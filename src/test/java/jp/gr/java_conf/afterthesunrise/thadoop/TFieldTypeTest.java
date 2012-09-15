01cfa5cfac63fd741f45624e9c89513e66c5ca37
Takanori Takase <subac1008-git@yahoo.co.jp>
2012-09-15 17:05:35 +0900

    Added thrift field enum.

---
+++ src/test/java/jp/gr/java_conf/afterthesunrise/thadoop/TFieldTypeTest.java
@@ -0,0 +1,63 @@
+package jp.gr.java_conf.afterthesunrise.thadoop;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+
+import org.apache.thrift.protocol.TType;
+import org.junit.Test;
+
+/**
+ * @author takanori.takase
+ */
+public class TFieldTypeTest {
+
+	@Test
+	public void testFind() {
+
+		for (TFieldType type : TFieldType.values()) {
+			assertSame(type, TFieldType.find(type.getId()));
+		}
+
+		assertNull(TFieldType.find(Byte.MIN_VALUE));
+
+		assertNull(TFieldType.find(Byte.MAX_VALUE));
+
+	}
+
+	@Test
+	public void testCoverage() throws Exception {
+
+		Field[] fields = TType.class.getFields();
+
+		int count = 0;
+
+		for (Field f : fields) {
+
+			int modifier = f.getModifiers();
+
+			if (!Modifier.isPublic(modifier)) {
+				continue;
+			}
+
+			if (!Modifier.isStatic(modifier)) {
+				continue;
+			}
+
+			byte id = (byte) f.get(null);
+
+			assertNotNull(f.getName(), TFieldType.find(id));
+
+			count++;
+
+		}
+
+		assertEquals(TFieldType.values().length, count);
+
+	}
+
+}
